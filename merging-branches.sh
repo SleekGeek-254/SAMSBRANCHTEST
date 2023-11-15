c0aa39ebfcec5b935424888405c900cd89914925
SleekGeek254 <tchiwo@icloud.com>
2023-11-15 10:22:41 +0300

    Merge logic

---
+++ merging-branches.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Prompt the user to select the directory for subtree split
+echo "Which branch would you like to merge:"
+read selected_branch
+
+
+git checkout master
+
+# Prompt the user if they want to push the changes to the remote repository
+echo "Do you want to merge the changes to from branch repository? (y/n) "
+read merge_response
+
+if [ "merge_response" == "y" or  "merge_response" == "Y" ]; then
+    # Push changes to the remote repository
+    git merge --allow-unrelated-histories -X theirs "$selected_branch"
+fi
+
+# Commit the changes
+git add "$selected_branch"/
+git commit -m "Merge "$selected_branch" into Master branch"
+
+
+# Prompt the user if they want to push the changes to the remote repository
+echo "Do you want to push the changes to the remote repository? (y/n)"
+read push_response
+
+if [ "$push_response" == "y" ]; then
+    # Push changes to the remote repository
+    git push origin master
+fi
+
+# Display a message indicating the process is complete
+echo "Merge Complete"
